dd6c92081ea38fc8a398aa4ceaca36e9eae675f8
zhangmingyang <1154540719@qq.com>
2018-12-10 14:26:55 +0800

    首页的发帖的遍历

---
+++ src/main/java/com/neusoft/controller/IndexController.java
@@ -0,0 +1,25 @@
+package com.neusoft.controller;
+
+import com.neusoft.mapper.TopicMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class IndexController {
+    @Autowired
+    TopicMapper topicMapper;
+    @RequestMapping("/")
+    public ModelAndView index(){
+    List<Map<String,Object>> mapList = topicMapper.getAllTopics();
+    ModelAndView modelAndView = new ModelAndView();
+    modelAndView.setViewName("index");
+    modelAndView.addObject("topics",mapList);
+
+    return modelAndView;
+}
+}
